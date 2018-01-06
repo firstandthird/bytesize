99457219673fc386ad72bd2445b3b467fd72e570
Chris <orthagonal1@gmail.com>
2018-01-05 18:43:11 -0600

    really work that async/await

--- index.js
+++ index.js
@@ -2,30 +2,24 @@ const zlib = require('zlib');
 const gzip = zlib.createGzip();
 const fs = require('fs');
 const prettyBytes = require('pretty-bytes');
+const util = require('util');
 
 const stringSize = (str, pretty) => {
   let bytes = Buffer.byteLength(str);
   return (pretty) ? prettyBytes(bytes) : bytes;
 };
 
-const fileSize = (file, pretty, gzip) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(file, 'utf8', function(err, file) {
-      if (gzip) {
-        zlib.gzip(file, function(err, buf) {
-          if (err) {
-            return reject(err);
-          }
-          bytes = buf.length;
-          bytes = (pretty) ? prettyBytes(bytes) : bytes;
-          resolve(bytes);
-        });
-      } else {
-        resolve(stringSize(file, pretty));
-      }
-    });
-  });
+const fileSize = async(file, pretty, gzip) => {
+  fileData = await util.promisify(fs.readFile)(file, 'utf8');
+  if (gzip) {
+    const buf = await util.promisify(zlib.gzip)(fileData);
+    bytes = buf.length;
+    bytes = (pretty) ? prettyBytes(bytes) : bytes;
+    return bytes;
+  }
+  return stringSize(fileData, pretty);
 };
+
 const gzipSize = async(file, pretty) => {
   return fileSize(file, pretty, true);
 };
