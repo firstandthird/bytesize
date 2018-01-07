843b6aaf8365e35c31eb1a4b7027c515ba0b9680
orthagonal <orthagonal1@gmail.com>
2018-01-07 14:50:26 -0600

    gzipStringSize

--- test/bytesize.test.js
+++ test/bytesize.test.js
@@ -6,6 +6,7 @@ tap.test('stringSize', (t) => {
   t.equal(size, 14);
   t.end();
 });
+
 tap.test('pretty stringSize', (t) => {
   const size = bytesize.stringSize('1 12 3 123 123', true);
   t.equal(size, '14 B');
@@ -33,5 +34,13 @@ tap.test('gzipSize', async(t) => {
 tap.test('pretty gzipSize', async(t) => {
   const size = await bytesize.gzipSize(`${__dirname}/fixtures/test.txt`, true);
   t.equal(size, '190 B');
+  t.end();
+});
+
+tap.test('gzipstringsize', async(t) => {
+  const size = await bytesize.gzipStringSize('1 12 3 123 123 123 35 12 1 12', false);
+  t.equal(size, 35);
+  const prettySize = await bytesize.gzipStringSize('1 12 3 123 123 123 35 12 1 12', true);
+  t.equal(prettySize, '35 B');
   t.end();
 });
